f7855fd2df981dfb42a626d2f74c974580ec6888
Gaurav <gauravm.git@gmail.com>
2023-08-25 15:44:31 +0530

    Added new code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the line");
+
+    println!("The secret number is: {secret_number}");
+    println!("You guessed: {guess}");
 }
